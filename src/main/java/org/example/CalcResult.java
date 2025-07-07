d96eb8084af5710482f37c2515f24414e6312c73
kaitheengineer <93702715+baaska23@users.noreply.github.com>
2025-07-07 15:25:53 +0800

    Add source code files

---
+++ src/main/java/org/example/CalcResult.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class CalcResult {
+    public int counter;
+    public boolean isGameOver;
+    public int maxAttempt;
+
+    public CalcResult(int counter, boolean isGameOver, int maxAttempt) {
+        this.counter = counter;
+        this.isGameOver = isGameOver;
+        this.maxAttempt = maxAttempt;
+    }
+}
